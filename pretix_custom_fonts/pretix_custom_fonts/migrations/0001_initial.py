5371eb73fa7bd1decae4cb36b2117c4f97573c59
linolaske <linolaske@icloud.com>
2026-03-15 12:55:55 +0100

    change

--- pretix_custom_fonts/pretix_custom_fonts/migrations/0001_initial.py
+++ pretix_custom_fonts/pretix_custom_fonts/migrations/0001_initial.py
@@ -1,6 +1,6 @@
 from django.db import migrations, models
 import django.db.models.deletion
-import pretix_custom_fonts.pretix_custom_fonts.models
+import pretix_custom_fonts.models
 
 class Migration(migrations.Migration):
 
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('name', models.CharField(help_text='A descriptive name for this font.', max_length=255, verbose_name='Font name')),
-                ('font_file', models.FileField(help_text='Only TTF and OTF files are supported.', upload_to=pretix_custom_fonts.pretix_custom_fonts.models.font_path, verbose_name='Font file')),
+                ('font_file', models.FileField(help_text='Only TTF and OTF files are supported.', upload_to=pretix_custom_fonts.models.font_path, verbose_name='Font file')),
                 ('organizer', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='custom_fonts', to='pretixbase.organizer')),
             ],
         ),
